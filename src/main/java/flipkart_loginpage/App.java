8ee688f7fbd595b351b6a2ca0a78008db65969f1
rahuldass <rahuldasdasari@gmail.com>
2022-07-07 12:31:18 +0530

    frontend login page is created

---
+++ src/main/java/flipkart_loginpage/App.java
@@ -0,0 +1,13 @@
+package flipkart_loginpage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
